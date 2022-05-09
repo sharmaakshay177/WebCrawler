5b3065ffb76d477a8b0bd9e74246b2c0013af5d4
sharmaakshay177 <sharmaakshay177@gmail.com>
2022-05-09 18:44:01 +0530

    added webcrawler payload response definition.

--- src/main/scala/org/zio/example/webcrawler/model/CrawledResponse.scala
+++ src/main/scala/org/zio/example/webcrawler/model/CrawledResponse.scala
@@ -1,7 +1,10 @@
 package org.zio.example.webcrawler.model
 
 import org.jsoup.nodes.Element
+import zio.Ref
 import zio.json.{DeriveJsonCodec, JsonCodec}
+
+import scala.collection.mutable.ListBuffer
 
 // Responses
 final case class Elements(head: Element, body: Element)
@@ -19,9 +22,11 @@ object CrawledResponse {
   implicit val codec: JsonCodec[CrawledResponse] = DeriveJsonCodec.gen[CrawledResponse]
 }
 
-final case class EntirePayloadResponse(responses: List[CrawledResponse], error: Option[String])
-object EntirePayloadResponse {
-  implicit val codec: JsonCodec[EntirePayloadResponse] = DeriveJsonCodec.gen[EntirePayloadResponse]
+final case class ConvertedResponse(responseOrError: Either[Option[String], CrawledResponse])
+
+final case class EntirePayload(val responses: Ref[ListBuffer[CrawledResponse]], val errors: Ref[ListBuffer[String]])
+object EntirePayload {
+  implicit val codec: JsonCodec[EntirePayload] = DeriveJsonCodec.gen[EntirePayload]
 }
 
 // Requests
--- src/main/scala/org/zio/example/webcrawler/services/WebCrawlerService.scala
+++ src/main/scala/org/zio/example/webcrawler/services/WebCrawlerService.scala
@@ -2,24 +2,22 @@ package org.zio.example.webcrawler.services
 
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
-import org.zio.example.webcrawler.model.{CrawledData, CrawledResponse, Elements, EntirePayloadResponse}
-import zio.{Function1ToLayerOps, Task, UIO, URLayer, ZIO}
+import org.zio.example.webcrawler.model._
+import zio.{Function1ToLayerOps, Ref, Task, UIO, URLayer, ZIO}
 
+import scala.collection.mutable.ListBuffer
 import scala.util.Try
 
 // https://scalac.io/blog/introduction-to-programming-with-zio-functional-effects/
 
 trait WebCrawlerService {
-  def getCrawledData(url: String): Task[CrawledResponse]
-  def getPayloadResponse(urls: List[String]): Task[EntirePayloadResponse]
+  def getPayloadResponse(urls: List[String]): Task[EntirePayload]
   def pong: UIO[String]
 }
 
 object WebCrawlerService {
-  def getCrawledData(url: String): ZIO[WebCrawlerService, Throwable, CrawledResponse] =
-    ZIO.serviceWithZIO[WebCrawlerService](_.getCrawledData(url))
 
-  def getPayloadResponse(urls: List[String]): ZIO[WebCrawlerService, Throwable, EntirePayloadResponse] =
+  def getPayloadResponse(urls: List[String]): ZIO[WebCrawlerService, Throwable, EntirePayload] =
     ZIO.serviceWithZIO[WebCrawlerService](_.getPayloadResponse(urls))
 
   def pong: ZIO[WebCrawlerService, Nothing, String] =
@@ -30,12 +28,49 @@ final case class WebCrawlerServiceLive(cache: ZioCacheService) extends WebCrawle
 
   override def pong: UIO[String] = UIO.succeed("pong")
 
-  override def getPayloadResponse(
-    urls: List[String],
-  ): Task[EntirePayloadResponse] =
-    ???
+  override def getPayloadResponse(urls: List[String]): Task[EntirePayload] =
+    for {
+      effects   <- ZIO.succeed(urls.map(Task.attempt(_)))
+      processed <- Task.foreachPar(effects)(_.map(cacheOrCall))
+      interim <- Task.attempt(
+        processed.map(
+          _.fold[ConvertedResponse](
+            err => ConvertedResponse(Left(Some(err.getMessage))),
+            resp => ConvertedResponse(Right(resp))
+          )
+        )
+      )
+      all            <- ZIO.collectAll(interim)
+      localResponses <- Ref.make(ListBuffer.empty[CrawledResponse])
+      localErrors    <- Ref.make(ListBuffer.empty[String])
+      payload = EntirePayload(localResponses, localErrors)
+      _ <- collate(all, payload)
+    } yield payload
 
-  override def getCrawledData(url: String): Task[CrawledResponse] =
+  private def collate(convertedResponse: List[ConvertedResponse], payload: EntirePayload): UIO[Unit] = {
+    def errorHelper(error: Option[String], payload: EntirePayload): UIO[Unit] =
+      for {
+        oldErrorList   <- payload.errors.get
+        errorExtracted <- ZIO.attempt(error.get) orElse ZIO.succeed("")
+        errorListNew = if (errorExtracted == "") oldErrorList else oldErrorList.addOne(errorExtracted)
+        _ <- payload.errors.set(errorListNew)
+      } yield ()
+
+    def responseHelper(response: CrawledResponse, payload: EntirePayload): UIO[Unit] =
+      for {
+        oldResponseList <- payload.responses.get
+        _               <- payload.responses.set(oldResponseList.addOne(response))
+      } yield ()
+
+    for {
+      _ <- UIO.foreach(convertedResponse)(_.responseOrError match {
+        case Left(value)  => errorHelper(value, payload)
+        case Right(value) => responseHelper(value, payload)
+      })
+    } yield ()
+  }
+
+  private def getCrawledData(url: String): Task[CrawledResponse] =
     for {
       document        <- scrapUrl(url)
       crawledResponse <- prepareData(url, document)
